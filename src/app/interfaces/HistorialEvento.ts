49511363e0b5d29f1517442b9066a20a773535be
german <27436197+LuisGermanDev@users.noreply.github.com>
2024-06-18 10:44:06 -0400

    colunma acciones listo

---
+++ src/app/interfaces/HistorialEvento.ts
@@ -0,0 +1,8 @@
+export interface HistorialEvento{
+    NombreEvento:string;
+    Descripcion:string;
+    Ubicacion:string;
+    Contacto:string;
+    Tipo:string;
+    FechaDetalle:string;
+}
--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,5 +1,5 @@
 import { Component, inject } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import {MatCardModule} from '@angular/material/card';
 import {MatTableModule} from '@angular/material/table';
 
@@ -8,10 +8,11 @@ import { Evento } from '../../interfaces/Evento';
 import { Router } from '@angular/router';
 import {MatInputModule} from '@angular/material/input';
 import {MatButtonModule} from '@angular/material/button';
+import { HistorialEvento } from '../../interfaces/HistorialEvento';
 @Component({
   selector: 'app-inicio',
   standalone: true,
-  imports: [MatCardModule,MatTableModule,MatButtonModule,MatInputModule],
+  imports: [MatCardModule,MatTableModule,MatButtonModule,MatInputModule,CommonModule],
   templateUrl: './inicio.component.html',
   styleUrl: './inicio.component.css'
 })
@@ -19,10 +20,16 @@ export class InicioComponent {
 
     private eventoService = inject(EventoService); 
     public listaEvento:Evento[]=[];
-    public displayedColumns:string[]=["nombre_evento","descripcion","ubicacion","contacto","detalle_fecha"]
+    public displayedColumns:string[]=["nombre_evento","descripcion","ubicacion","contacto","tipo","detalle_fecha",'acciones']
+
+
+    // public listaHistorial:HistorialEvento[]=[];
+    // public displayedColumnsHisEvn:string[]='NombreEvento','Descripcion','Ubicacion','Contacto','Tipo','FechaDetalle']
+
+    
     
     private router = inject(Router);
-
+    Pestana: number = 1; 
     constructor(){
       this.eventoService.lista().subscribe({
         next:(data)=>{
@@ -37,6 +44,19 @@ export class InicioComponent {
       })
 
     }
+    Eventos(){
+      this.Pestana=1;
+    }
+    HistorialEvento(){
+      this.Pestana=2;
+    }
+    HistorialEventoInviduales(){
+      this.Pestana=3;
+    }
+    HistorialEventogrupales(){
+      this.Pestana=4;
+    }
+
     // funcion salir
   Salir(){
     this.router.navigate(['']);
